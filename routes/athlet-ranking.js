131a299b6dfc7ad493fdc156982a04a57ba6e9f6
Iwan Bolzern <iwan.bolzern@bluewin.ch>
2017-12-17 20:01:07 +0100

    begin implementation of athlet-ranking

--- routes/athlet-ranking.js
+++ routes/athlet-ranking.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var database = require('./utils/database');
 
-router.get('/:discipline', function(req, res, next) {
+function createAthletView(athlet, dis) {
+    let viewAthlet = {
+        ID: athlet._id,
+        Name: athlet.FamilyName,
+        Nationality: athlet.Organisation
+    };
+    var average = arr => arr.reduce( ( p, c ) => p + c, 0 ) / arr.length;
 
+    let avg = average(dis.Qualifications);
+    let avgSeries = [];
+    let minLength = Math.min.apply(Math, dis.Qualifications.map(x => x.Series.length));
+    for(let i = 0; i < minLength; i++) {
+        let avg = average(dis.Qualifications.map(x => x.Series[0]));
+        avgSeries.append(avg);
+    }
+
+    let result = regression.linear(dis.Qualifications.map(x => x.Score));
+    let gradient = result.equation[0];
+    //ToDO; arrange to up, down...
+
+    viewAthlet.AvgScore = avg;
+    viewAthlet.AvgSeries = avgSeries;
+}
+
+router.get('/:discipline', async function(req, res, next) {
+    // get params
     var dis = req.params.discipline;
+
+    //get data
+    athlets = await database.getAthlets(dis);
+    athlets.forEach(athlet => {
+        let dis = athlet.Disciplines.filter(dis => dis.Name == dis)[0];
+        let athletView = createAthletView(athlet, dis);
+
+    });
 
 
     result = [
--- utils/database.js
+++ utils/database.js
@@ -1,23 +1,24 @@
 var MongoClient = require('mongodb').MongoClient;
 var database = null;
 
-var initialize = function(config) {
+var initialize = async function(config) {
     // Connection URL
     var url = config.connection_string;
 
     // Use connect method to connect to the Server
-    MongoClient.connect(url, function(err, db) {
-        if(err)
-            console.error("Connection to MongoDB Server faild");
-
-        database = db
-    });
+    database = await MongoClient.connect(url);
 };
 
-var getAthlets = function(disciplinId) {
-    var collection = database.collection('athlet');
+var getAthlets = async function(disciplinId) {
+    let collection = database.collection('athlet');
+    let athlets = await collection.find({'Disziplines':
+            { $elemMatch : {'Name': disciplinId}}
+    }).toArray();
+
+    return athlets;
 };
 
 module.exports = {
+    getAthlets: getAthlets,
     initialize: initialize
 };
