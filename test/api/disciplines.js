6f98a8740692016fef41df85197558779c74575f
Lucien Zürcher <l.zuercher@tincan.ch>
2017-12-17 14:03:25 +0100

    added test environment with test and watch command aswell some supportive libraries

---
+++ test/api/disciplines.js
@@ -0,0 +1,26 @@
+const request = require('supertest');
+const app = require('../../app');
+const chai = require('chai');
+const expect = chai.expect;
+
+chai.use(require('chai-http'));
+chai.use(require('chai-json-schema'));
+
+describe('Test disciplines route', () => {
+  it('GET /disciplines validate JSON-Schema', done => {
+    request(app)
+      .get('/disciplines')
+      .expect(200)
+      .end((err, res) => {
+        expect(res).to.be.json;
+        expect(res.body).to.be.jsonSchema({
+          type: 'array',
+          items: {
+            type: "string"
+          },
+          'uniqueItems': true
+        });
+        done();
+      })
+  });
+});
