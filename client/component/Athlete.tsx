ec7461ad00dc0f6a5466a9b03c5759a992cdabc4
Philipp Bütler <philipp.buetler@trivelop.ch>
2017-12-20 20:12:06 +0100

    Display athlete values

--- client/component/Athlete.tsx
+++ client/component/Athlete.tsx
@@ -7,10 +7,37 @@ interface IRankingProps {
 }
 
 export class Athlete extends React.Component<IRankingProps> {
-  render() {
-    return (
-      <div>Athlete {this.props.athlete.FamilyName} {this.props.athlete.GivenName}</div>
-    )
+    render() {
+
+        const asdf = this.props.athlete.Disciplines.map(r => <div>
+            <h1>-----{r.Name}-----</h1>
+            <h2>Qualifikation</h2>
+            {r.Qualifications.map(d => <div>
+                {d.Competition}
+                {d.Series.map(s => <div>
+                    {s}
+                    </div>
+                )}
+            </div>)}
+            <h2>Finals</h2>
+            {r.Finals.map(f =>
+                <div>
+                    {f.Competition}
+                    {f.Series.map(s => <div>
+                        {s}
+                    </div>
+                    )}
+                </div>)}
+        </div>);
+
+      return (
+          <div>
+              <div>Athlete {this.props.athlete.FamilyName} {this.props.athlete.GivenName}</div>
+              <div>
+                  {asdf}
+              </div>
+         </div>
+      )
   }
 }
 
