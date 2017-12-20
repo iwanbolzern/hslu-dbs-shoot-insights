615a1bf25323092c12b0a882bc5bac313df1255c
Lucien Zürcher <lucien.zuercher@hotmail.com>
2017-12-20 20:48:32 +0100

    Update athlet.js

--- routes/athlet.js
+++ routes/athlet.js
@@ -9,9 +9,13 @@ router.get('/:id', async function(req, res, next) {
     let dis = [];
     let status = 200;
 
+    // Verbinden mit der MongoDB 
     let db = await MongoClient.connect(config.connection_string);
     try {
+      // Datenbank Selektieren 
       let dbBase = db.db('shoot-insights');
+      // Collection "athlet" Selektieren und durch findOne() den Cursor erhalten und den ersten Wert  
+      // zurückgeben 
       var athlet = await dbBase.collection("athlet").findOne(ObjectID(id));
     } catch(ex) {
       status = 404;
